1a110f17291b1184f54b253dd9a36eb189d4fe8e
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-07-21 08:31:12 +0200

    Parse incoming req & exec server side code

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,7 +1,27 @@
+import fs from 'fs';
+import path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'I am alive' });
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedback = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedback,
+    };
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(String(fileData));
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ status: 'Success', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'I am alive' });
+  }
 };
 
 export default handler;
